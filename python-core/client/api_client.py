d9f7c00589f3fd1cf2bd1cfc043edf4e0ba7e92f
IgorZ <694@ukr.net>
2017-02-01 00:09:19 +0200

    spring-core: dev: Temp client py-file

--- python-core/client/api_client.py
+++ python-core/client/api_client.py
@@ -17,8 +17,24 @@ data = {"uid": 2,
 headers = {'Content-Type': 'application/json'}
 url = 'http://localhost:8080/stats/server/save'
 
-
+# headers = {'content-type': 'application/json'}
 # r = requests.post(url, json=data, headers=headers)
+# print r.text
+
 r = requests.post(url, data=json.dumps(data))
 
 print r.text
+print r.status_code
+
+# import urllib
+# import os
+#
+# svr_mapurl = "http://localhost:8080/world"
+# mapName = "xr_test"
+#
+# online = urllib.urlopen('%s/%s.s2z' % (svr_mapurl, mapName))
+# meta = online.info()
+# print meta
+
+# localSize = os.path.getsize('E:\\Code\\Projects\\XR_Server\\spring-core\\world\\xr_test.s2z')
+# print localSize
