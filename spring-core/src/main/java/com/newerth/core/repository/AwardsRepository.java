a097d281bea7022ab53110600ceb55b386254e26
Igor-ua <694@ukr.net>
2017-02-28 12:13:31 +0200

    Selecting from Top stats excluding 0 values

--- spring-core/src/main/java/com/newerth/core/repository/AwardsRepository.java
+++ spring-core/src/main/java/com/newerth/core/repository/AwardsRepository.java
@@ -9,21 +9,27 @@ import java.util.List;
 
 @Repository
 public interface AwardsRepository extends JpaRepository<Awards, Long> {
-	@Query(value = "SELECT * FROM awards ORDER BY accumulated_aimbot DESC LIMIT 5", nativeQuery = true)
+	@Query(value = "SELECT * FROM awards WHERE accumulated_aimbot > 0 ORDER BY accumulated_aimbot DESC LIMIT 5",
+			nativeQuery = true)
 	List<Awards> findTopAimbots();
 
-	@Query(value = "SELECT * FROM awards ORDER BY accumulated_mvp DESC LIMIT 5", nativeQuery = true)
+	@Query(value = "SELECT * FROM awards WHERE accumulated_mvp > 0 ORDER BY accumulated_mvp DESC LIMIT 5",
+			nativeQuery = true)
 	List<Awards> findTopMvps();
 
-	@Query(value = "SELECT * FROM awards ORDER BY accumulated_sadist DESC LIMIT 5", nativeQuery = true)
+	@Query(value = "SELECT * FROM awards WHERE accumulated_sadist > 0 ORDER BY accumulated_sadist DESC LIMIT 5",
+			nativeQuery = true)
 	List<Awards> findTopSadists();
 
-	@Query(value = "SELECT * FROM awards ORDER BY accumulated_ripper DESC LIMIT 5", nativeQuery = true)
+	@Query(value = "SELECT * FROM awards WHERE accumulated_ripper > 0 ORDER BY accumulated_ripper DESC LIMIT 5",
+			nativeQuery = true)
 	List<Awards> findTopRippers();
 
-	@Query(value = "SELECT * FROM awards ORDER BY accumulated_phoe DESC LIMIT 5", nativeQuery = true)
+	@Query(value = "SELECT * FROM awards WHERE accumulated_phoe > 0 ORDER BY accumulated_phoe DESC LIMIT 5",
+			nativeQuery = true)
 	List<Awards> findTopPhoes();
 
-	@Query(value = "SELECT * FROM awards ORDER BY accumulated_survivor DESC LIMIT 5", nativeQuery = true)
+	@Query(value = "SELECT * FROM awards WHERE accumulated_survivor > 0 ORDER BY accumulated_survivor DESC LIMIT 5",
+			nativeQuery = true)
 	List<Awards> findTopSurvivors();
 }
