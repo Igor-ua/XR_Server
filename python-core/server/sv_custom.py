10865dc4ed9a122ca47222f87c5170b1b14fa0bc
Igor-ua <694@ukr.net>
2017-03-06 13:26:29 +0200

    Changes in configs

--- python-core/server/sv_custom.py
+++ python-core/server/sv_custom.py
@@ -12,6 +12,8 @@ import sv_custom_utils
 def init():
     print("[!]   Initializing Custom Module...")
     try:
-        cmd_converter.init()
+        pass
+        # Notice: server.serve_forever() from cmd_converter doesn't let server to reboot itself
+        # cmd_converter.init()
     except:
         sv_custom_utils.simple_exception_info()
