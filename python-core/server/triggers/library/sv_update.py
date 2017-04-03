70748f51c38d56089d721b59b07a3556da9a5be1
IgorZ <694@ukr.net>
2017-04-03 20:20:46 +0300

    Linux updater bugfix

--- python-core/server/triggers/library/sv_update.py
+++ python-core/server/triggers/library/sv_update.py
@@ -1,6 +1,7 @@
 # ---------------------------------------------------------------------------
 #           Name: sv_update.py
-#    Description: Checks and updates in the end of the game
+#    Description: Updated that works in the end of the game.
+#                 Should be used for Linux servers.
 # ---------------------------------------------------------------------------
 
 # Savage API
@@ -12,8 +13,8 @@ import urllib
 import sys
 
 # Variables ---------------------
-need_to_update = False
 run_checkers = True
+run_once = True
 local_version = 0
 remote_version = 0
 remote_url = 'http://www.newerth.com/autoupdater/production/linux/server/autoupdater/version.txt'
@@ -24,17 +25,19 @@ local_file_path = '../autoupdater/localversion/version.txt'
 # -------------------------------
 def check():
     global run_checkers
-    global need_to_update
+    global run_once
     # If the game has ended - check remote and local versions of the server
     if server.GetGameInfo(GAME_STATE) == 4 and run_checkers:
         run_checkers = False
+        core.CvarGetValue('svr_update_available', 0)
         check_local_version()
         check_remote_version()
         compare_versions()
     # If the game is about to load/restart a map server does own update
-    if server.GetGameInfo(GAME_STATE) > 4 and need_to_update:
-        need_to_update = False
-        return 1
+    if server.GetGameInfo(GAME_STATE) > 4 and run_once:
+        run_once = False
+        if int(core.CvarGetValue('svr_update_available')):
+            return 1
     return 0
 
 
@@ -59,14 +62,14 @@ def check_remote_version():
 
 
 def compare_versions():
-    global need_to_update
     core.ConsolePrint("Checking version: local is: %s, remote is: %s\n" % (local_version, remote_version))
     if remote_version > local_version:
-        need_to_update = True
+        core.CvarSetValue('svr_update_available', 1)
         # Notify all clients that update will take place
         server.Notify(-1, "^779(XR AutoUpdater) ^wThe server will restart to apply XR's latest updates.")
         server.Notify(-1, "^779(XR AutoUpdater) ^wPlease Wait! You will automatically reconnect if you have the latest version of XR.")
     elif local_version == remote_version:
+        core.CvarSetValue('svr_update_available', 0)
         core.ConsolePrint("Local and remote versions are the same.")
 
 
